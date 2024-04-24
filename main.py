faba33fa94e9cefab89b112422de0352abf7bb10
adriano12341 <163434738+adriano12341@users.noreply.github.com>
2024-04-24 11:49:35 +0200

    wip: added update function

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from models.data_source import users
-from utils.crud import add_user
-from utils.crud import read_friends, search_user, remove_user
-from utils.emap import single_map
-
+from utils.crud import read_friends, add_user, search_user, remove_user, update_user
+from utils.emap import single_map, full_map
 
 if __name__ == '__main__':
     while True:
-        print("Welcome to the menu choose an option:")
-        print("1. Read a friends list")
+        print("Welcome to the menu  ")
+        print("0. Exit ")
+        print("1. Read a list of friends ")
         print("2. Add new user")
         print("3. Search user")
         print("4. Remove user")
-        print("5. Generate map")
-        print("6. Exitdfgdhg")
-        print("0. Exit")
+        print("5. Update user")
+        print("6. Generate single map")
+        print("7. Generate full map")
         menu_option = input("Choose an option:")
         if menu_option == "0":
             break
@@ -26,5 +25,9 @@ if __name__ == '__main__':
         if menu_option == "4":
             remove_user(users)
         if menu_option == "5":
-            single_map(search_user(users)['location'])
+            update_user(users)
         if menu_option == "6":
+            single_map(search_user(users)['location'])
+            print(search_user(users)['location'])
+        if menu_option == "7":
+            full_map(users)
--- utils/emap.py
+++ utils/emap.py
@@ -1,16 +1,28 @@
+import folium
 import requests
 from bs4 import BeautifulSoup
-import folium
-from models.data_source import users
 def single_map(user_location:str)->None:
     url: str = f'https://pl.wikipedia.org/wiki/{user_location}'
     response = requests.get(url)
     response_html = BeautifulSoup(response.text, 'html.parser')
-    latitude = response_html.select(".latitude")[1].text.replace(",", ".")
-    longitude = response_html.select(".longitude")[1].text.replace(",", ".")
-    print(latitude)
-    print(longitude)
+    latitude = response_html.select('.latitude')[1].text.replace(",",".")
+    longitude = response_html.select('.longitude')[1].text.replace(",",".")
+    map = folium.Map(location=[latitude, longitude], zoom_start=11)
+    folium.Marker(location=[latitude, longitude],popup=f"{user_location} rządzi!!?? ").add_to(map)
+    map.save(f'./{user_location}.html')
 
-    map = folium.Map(location=[latitude, longitude], zoom_start=13)
-    folium.Marker(location=[latitude, longitude],popup=f"{user_location} rządzi").add_to(map)
-    map.save(f"./{user_location}.html")
+
+def full_map(users:str)->None:
+    lista_wspolrzednych = []
+    map = folium.Map(location=[52, 21], zoom_start=8)
+    for user in users:
+        url: str = f'https://pl.wikipedia.org/wiki/{user['location']}'
+        response = requests.get(url)
+        response_html = BeautifulSoup(response.text, 'html.parser')
+        latitude = response_html.select('.latitude')[1].text.replace(",",".")
+        longitude = response_html.select('.longitude')[1].text.replace(",",".")
+        lista_wspolrzednych.append([latitude, longitude])
+    for wspolrzedne in lista_wspolrzednych:
+        folium.Marker(location= wspolrzedne ).add_to(map)
+
+    map.save(f'./common_map.html')
